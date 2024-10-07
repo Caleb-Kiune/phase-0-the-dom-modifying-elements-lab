c200c3ecfb1d23c29f19c6372c15b1b959bb1d0b
Caleb <calebkiune@gmail.com>
2024-10-07 19:29:37 +0300

    Adds lab files

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let element = document.getElementById('main')
+element.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "Caleb is the champion"
